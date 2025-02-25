60617cc0cbbd8e49ea37e08d74bc966455e5fdf6
Oumayma El-fahsi <ooumayma>
2025-02-25 12:31:16 +0000

    add some useless code

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -1,7 +1,7 @@
 
 //create a function that englobes all the functions of the validation process.
 function validation(puzzle, words) {
-  if (!puzzle||!words) {
+  if (!puzzle || !words) {
     return null
   }
   let cleanPuzzle = validatePuzzle(puzzle)
@@ -9,35 +9,14 @@ function validation(puzzle, words) {
 }
 
 
-const puzzle = `...1...l...1....
-..1000001000...
-...0....0......
-.1..1...0...1..
-.0....100000000
-100000..0...0..
-.0.....1001000.
-.0.1....0.0....
-.10000000.0....
-.0.0......0....
-.0.0.....100...
-...0......0....
-..........0....`
+const puzzle = `2001
+0..0
+1000
+0..0`
+const words = ['casa', 'alan', 'ciao', 'anta']
 
-const words = [
-  'sun',
-  'sunglasses',
-  'Swimming',
-  'Bikini',
-  'beach',
-  'icecream',
-  'tan',
-  'deckchair',
-  'sand',
-  'seaside',
-  'saQQSDDDndals',
-]
 
-console.log(validation(puzzle,words));
+
 // Create a function to validate and split the input lines of the puzzle:
 function SplitAndVAlidate(line) {
   let slc = []
@@ -93,8 +72,62 @@ function validateEntries(puzzle, words) {
 }
 // Declare the initial function to run the program:
 function crosswordSolver(puzzle, words) {
-  let cleanPuzzle =validation(puzzle,words)
+  let cleanPuzzle = validation(puzzle, words)
   if (!cleanPuzzle) {
     return "Error"
   }
 }
+
+console.log(validation(puzzle, words));
+
+// hadi will return true or false based on if 1 or 2 i there and the remaining to the left are 0 to a certainlimit
+function isValidateRow(two_dim_array, row_index, start_column) {
+  // return false and 0 otherwise true and length
+  let stop = start_column + 1;
+  let len = 1;
+  console.log(two_dim_array.length - start_column)
+  for (let i = stop; i < two_dim_array[row_index].length - start_column; i++) {
+    if (i !== stop && two_dim_array[row_index][i] === ".") {
+      return [true, len]
+    } else if (i !== stop && i === (two_dim_array.length - start_column) - 1) {
+      return [true, len + 1]
+    }
+    else if (two_dim_array[row_index][i] !== ".") {
+      len = len + 1
+    } else if (i === stop && two_dim_array[row_index][i] === ".") {
+      return [false, 0]
+    }
+  }
+  return [false, 0]
+}
+
+
+let valid = validation(puzzle, words)
+console.log("Row validation",isValidateRow(valid, 0, 3))
+
+// nfss lblan dyal li 9blha 
+
+function isValidColumn(two_dim_array, start_row, column_index) {
+  let len = 1
+  console.log("len of the columns",two_dim_array.length)
+  let stop = start_row + 1
+  console.log(two_dim_array.length - start_row)
+  for (let i = stop; i < two_dim_array.length - start_row; i++) {
+    if (i !== stop && two_dim_array[column_index][i] === ".") {
+      return [true, len]
+    } else if (i !== stop && i === (two_dim_array.length - start_row) - 1) {
+      return [true, len + 1]
+    }
+    else if (two_dim_array[column_index][i] !== ".") {
+      len = len + 1
+    } else if (i === stop && two_dim_array[column_index][i] === ".") {
+      console.log(two_dim_array[column_index][i])
+
+      return [false, 0]
+    }
+  }
+  return [false,0]
+
+}
+console.log("Column Validation:",isValidColumn(valid, 0, 0))
+
