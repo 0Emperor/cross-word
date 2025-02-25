463b2bdfce8e87c1936dbcbb72bcd331f66a03d7
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-25 14:03:20 +0000

    add some code

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -9,10 +9,10 @@ function validation(puzzle, words) {
 }
 
 
-const puzzle = `2001
+const puzzle = `2.00
 0..0
-1000
-0..0`
+2000
+00.0`
 const words = ['casa', 'alan', 'ciao', 'anta']
 
 
@@ -81,11 +81,10 @@ function crosswordSolver(puzzle, words) {
 console.log(validation(puzzle, words));
 
 // hadi will return true or false based on if 1 or 2 i there and the remaining to the left are 0 to a certainlimit
-function isValidateRow(two_dim_array, row_index, start_column) {
+function isValidRow(two_dim_array, row_index, start_column) {
   // return false and 0 otherwise true and length
   let stop = start_column + 1;
   let len = 1;
-  console.log(two_dim_array.length - start_column)
   for (let i = stop; i < two_dim_array[row_index].length - start_column; i++) {
     if (i !== stop && two_dim_array[row_index][i] === ".") {
       return [true, len]
@@ -102,32 +101,59 @@ function isValidateRow(two_dim_array, row_index, start_column) {
 }
 
 
-let valid = validation(puzzle, words)
-console.log("Row validation",isValidateRow(valid, 0, 3))
+
 
 // nfss lblan dyal li 9blha 
-
 function isValidColumn(two_dim_array, start_row, column_index) {
   let len = 1
-  console.log("len of the columns",two_dim_array.length)
   let stop = start_row + 1
-  console.log(two_dim_array.length - start_row)
   for (let i = stop; i < two_dim_array.length - start_row; i++) {
-    if (i !== stop && two_dim_array[column_index][i] === ".") {
+    console.log("i:", i);
+
+    if (i !== stop && two_dim_array[i][column_index] === ".") {
       return [true, len]
     } else if (i !== stop && i === (two_dim_array.length - start_row) - 1) {
       return [true, len + 1]
     }
-    else if (two_dim_array[column_index][i] !== ".") {
+    else if (two_dim_array[i][column_index] !== ".") {
       len = len + 1
-    } else if (i === stop && two_dim_array[column_index][i] === ".") {
-      console.log(two_dim_array[column_index][i])
-
+    } else if (i === stop && two_dim_array[i][column_index] === ".") {
+      console.log(two_dim_array[i][column_index])
       return [false, 0]
     }
   }
-  return [false,0]
+  return [false, 0]
 
 }
-console.log("Column Validation:",isValidColumn(valid, 0, 0))
+
+function isSafe(type, word, two_dim_array, row_index, column_index) {
+  switch (type) {
+    case "row":
+      const [ok, len] = isValidRow(two_dim_array, row_index, column_index)
+      if (ok) return word.length === len
+      else return false
+    case "column":
+      const [ok1, len1] = isValidColumn(two_dim_array, row_index, column_index)
+      if (ok1) return word.length === len1
+      else return false
+    default:
+      return false
+  }
+}
+
+
+
+
+
+function crosswordSolver(){
+
+}
+
+
+
+
+let valid = validation(puzzle, words)
+console.log("Row validation", isValidRow(valid, 0, 1))
+console.log("Column Validation:", isValidColumn(valid, 0, 0))
+console.log(isSafe("column","casa", valid, 0,0))
 
